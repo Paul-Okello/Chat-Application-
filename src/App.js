43bbc0c884d5db50a7ee11269cb71abb7706eb7f
Paul Okello <paulokello273@gmail.com>
2021-02-01 19:55:56 +0300

    render chatFeed on App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { ChatEngine } from "react-chat-engine";
 import "./App.css";
+import ChatFeed from "./components/ChatFeed";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       projectID="4ff83939-18cb-4e23-b223-5975f76f9923"
       userName="admin"
       userSecret="Pass123"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
 };
