25522afb4ab5c4faa34bb269de6d9609cc50e26e
Paul Okello <paulokello273@gmail.com>
2021-02-01 18:42:55 +0300

    Chat Engine setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="1d8b6530-98eb-4c4a-998c-82a81f82a932"
+      userName="admin"
+      userSecret="Pass123"
+    />
+  );
 };
 
 export default App;
